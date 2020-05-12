2746e3e1a6bbd65171b5fc8e393a42ddba84df42
Nikita Kulachenkov <kulacnikita@gmail.com>
2020-05-12 18:12:21 +0300

    New project, some additions

---
+++ 02bot.py
@@ -0,0 +1,62 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    def variants():
+        user_answer = int(input())
+        if user_answer == 2:
+            print("Completed, have a nice day!")
+            end()
+        else:
+            print("Please, try again")
+            variants()
+
+    print("""Let's find out what do you know about iZombie.
+    What is the main character's name?
+    1. Olivia Wild
+    2. Olivia Moore
+    3. Payton Charlez
+    4. Zhanna Arkadievna""")
+    variants()
+
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Andruha', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
